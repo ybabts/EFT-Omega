83c5fdc3a0a9c38955bfb7a8bb193dc0cdb4b874
Tyler Mueller <muelltyl@gmail.com>
2024-06-08 19:14:01 -0500

    adds a mod file server

--- website.ts
+++ website.ts
@@ -2,14 +2,39 @@ import { existsSync } from "https://deno.land/std@0.224.0/fs/exists.ts";
 
 import { default as omega } from "./omega.json" with { type: "json" }
 
+// get a list of directories in /mods
+const folders = Array.from(Deno.readDirSync("./mods"));
+// for each folder, look in /dist for a .zip file
+const mods = folders.map((folder) => {
+  const files = Array.from(Deno.readDirSync(`./mods/${folder.name}/dist`));
+  const mod = files.find((file) => file.name.endsWith(".zip"));
+  if (mod) {
+    return {
+      name: folder.name,
+      file: mod.name,
+    };
+  }
+  return null;
+}).filter((mod) => mod !== null);
+
+
 Deno.serve((req: Request) => {
   const url = new URL(req.url);
   if (url.pathname === "/modpack.json") {
     return new Response(JSON.stringify(omega))
   }
-  if (url.pathname.slice(0, 6) === "/mods/") {
-    if (!existsSync(`./mods/${url.pathname.slice(6)}`)) new Response("Mod does not exist", { status: 404 })
-    return new Response(Deno.readFileSync(`./mods/${url.pathname.slice(6)}`), { headers: { "Content-Type": "application/zip" } })
+  if (url.pathname.startsWith("/mods/")) {
+    const modname = url.pathname.slice(6);
+    const mod = mods.find((mod) => mod?.file === modname);
+    if (!mod) {
+      return new Response("Mod does not exist", { status: 404 });
+    }
+    const modFilePath = `./mods/${mod.name}/dist/${mod.file}`;
+    if (existsSync(modFilePath)) {
+      return new Response(Deno.readFileSync(modFilePath), { headers: { "Content-Type": "application/zip" } });
+    } else {
+      return new Response("Mod does not exist", { status: 404 });
+    }
   }
   return new Response("Not found", { status: 404 })
 })
