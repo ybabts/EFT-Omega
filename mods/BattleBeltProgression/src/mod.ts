2769ee6c2665a823cc89cbf1b6075add3cc6b00b
Tyler Mueller <muelltyl@gmail.com>
2024-06-10 19:06:48 -0500

    fixes omega

--- mods/BattleBeltProgression/src/mod.ts
+++ mods/BattleBeltProgression/src/mod.ts
@@ -1,12 +1,10 @@
 import { DependencyContainer } from "npm:tsyringe";
 import { DatabaseServer, IPostDBLoadMod } from "../deps.ts";
-import { TRADER_ID } from "https://deno.land/x/sptaki@1.2.0/mod.ts";
 
 class Mod implements IPostDBLoadMod {
   public postDBLoad(container: DependencyContainer): void {
     const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
     const traders = databaseServer.getTables().traders!;
-    traders[TRADER_ID]
   }
 }
 
